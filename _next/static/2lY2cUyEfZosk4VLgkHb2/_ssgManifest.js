849bb25e5c8971662da57a0ca0d4da11ade7b1b1
WANG Jinyuan <WangSteve@icloud.com>
2023-02-24 01:11:12 +0800

    add CNAME and update domain

---
+++ _next/static/2lY2cUyEfZosk4VLgkHb2/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[...listPathPage]","\u002Fmodels\u002F[slug]","\u002Funiverse\u002Fcategory\u002F[slug]","\u002Funiverse\u002Fproject\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
